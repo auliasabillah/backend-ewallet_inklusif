1acd500a08097839f3ebb1ae1c650915f5ce712b
auliasabillah <auliasabillah99@gmail.com>
2026-05-21 08:50:49 +0700

    api filter bulan, tanggal

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ Route::get('/riwayat/{user_id}', [TransaksiController::class, 'getRiwayat']);
 Route::post('/transfer', [WalletController::class, 'transfer']);
 Route::post('/payment', [WalletController::class, 'payment']);
 Route::put('/profile/{id}', [ProfileController::class, 'update']);
+Route::get('/pengeluaran/{user_id}', [TransaksiController::class, 'getPengeluaranHariIni']);
